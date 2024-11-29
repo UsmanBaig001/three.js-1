d1f3a5374757b9f6be0edc362da39577e774aca2
UsmanBaig001 <usmanbaig1572@gmail.com>
2024-11-29 23:04:45 +0500

    feat: added sharpness to the cube

--- script.js
+++ script.js
@@ -119,6 +119,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes?.width, sizes?.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 4));
 
 // Clock
 // const clock = new THREE.Clock();
