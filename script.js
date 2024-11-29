2e2502d79461f215a1e6acae3bb66073e724d8e0
UsmanBaig001 <usmanbaig1572@gmail.com>
2024-11-29 23:01:26 +0500

    feat: added debug panel

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import gsap from "gsap";
+
+//  Debug
+const gui = new dat.GUI();
+
+// Color
+const parameters = {
+  color: 0xff0000,
+  spin: () => {
+    gsap.to(mesh.rotation, {
+      duration: 6,
+      y: mesh.rotation.y + Math.PI * 2,
+    });
+  },
+};
 
 // Cursor position
-
 const cursor = {
   x: 0,
   y: 0,
@@ -54,20 +69,23 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1, 3, 3, 3);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  wireframe: true,
-  // side: THREE.DoubleSide,
-  wireframeLinewidth: 1,
-  // wireframeLinejoin: "round",
-  // wireframeLinecap: "round",
-});
+const geometry = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4);
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Debug
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01);
+gui.add(mesh.position, "x").min(-3).max(3).step(0.01);
+gui.add(mesh.position, "z").min(-3).max(3).step(0.01);
+gui.add(mesh, "visible");
+gui.add(material, "wireframe");
+gui.addColor(parameters, "color").onChange(() => {
+  material.color.set(parameters.color);
+});
+gui.add(parameters, "spin");
 // // Custom vertex shader
 // const positionArray = new Float32Array([
 //   0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1,
@@ -89,7 +107,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 3;
 // camera.position.y = 2;
 // camera.position.x = 2;
-camera.lookAt(mesh.position);
+// camera.lookAt(mesh.position);
 scene.add(camera);
 
 // Controls
@@ -118,7 +136,7 @@ const tick = () => {
   // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
   // camera.position.y = cursor.y * 3.5;
   controls.update();
-  camera.lookAt(mesh.position);
+  // camera.lookAt(mesh.position);
   // Render
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
